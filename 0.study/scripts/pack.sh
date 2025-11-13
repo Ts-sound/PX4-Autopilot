ee4a3453a51e993d1afb9aab56571858d5f18c41
Ts-sound <1564264931@qq.com>
2025-11-13 02:20:57 +0000

    temp

---
+++ 0.study/scripts/pack.sh
@@ -0,0 +1,2 @@
+
+tar -czf px4-autopilot.tar.gz --exclude=".git" --exclude="0.study" --exclude="build"  --exclude=".ci" --exclude=".clusterfuzzlite" --exclude=".vscode" --exclude=".devcontainer"  --exclude=".gitmodules" PX4-Autopilot
